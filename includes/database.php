7c00be80e821e2f3ac631cde11664833fbb43d66
Sheyilaaw <Sheyilaaw98@gmail.com>
2018-02-17 11:13:42 +0100

    database file updated to production

--- includes/database.php
+++ includes/database.php
@@ -1,9 +1,9 @@
 <?php
 defined('DB_SERVER') ? null : define('DB_SERVER', 'localhost');
 // defined('DB_USER')   ? null : define('DB_USER', 'id981499_sheyi');
-defined('DB_USER')   ? null : define('DB_USER', 'id4771856_sheyi');
+defined('DB_USER')   ? null : define('DB_USER', 'sheyi');
 defined('DB_PASS')   ? null : define('DB_PASS', 'nbvhgfytrm');
-defined('DB_NAME')   ? null : define('DB_NAME', 'id4771856_hackernest');
+defined('DB_NAME')   ? null : define('DB_NAME', 'hackernest');
 $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME) or die("Error Connecting to Database");
 
 if(isset($_SESSION['id'])){
