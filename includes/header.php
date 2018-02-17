0799d37cd1935e346267df7ce251564c3c784429
Luthfulahi <luteeph@gmail.com>
2018-02-17 11:14:36 +0100

    i dunno

--- includes/header.php
+++ includes/header.php
@@ -6,13 +6,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button> -->
-      <a class="navbar-brand" href="#">FishNet</a>
+      <a class="navbar-brand" href="index.html">FishNet</a>
     </div>
     <!-- <div class="collapse navbar-collapse" id="myNavbar"> -->
     <div>
       <ul class="nav navbar-nav  navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
     </div>
   </div>
